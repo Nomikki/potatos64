95b02c29a73a7b5b5c7af1396b2ec227dcfd4793
Kimon Kukulis <kimon.kukulis@gmail.com>
2024-10-05 20:11:23 +0300

    NULL added

--- include/stdio.h
+++ include/stdio.h
@@ -5,10 +5,12 @@
 #include <types.h>
 #include <stdlib.h>
 
+#define NULL 0
 #define EOF (-1)
 
 extern int puts(const char *str);
 extern int putchar(int ch);
 extern void printf(const char *format, ...);
+
 
 #endif
