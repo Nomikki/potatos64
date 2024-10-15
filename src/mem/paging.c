15ff44fa31812dd729a72cb0d78a693b5d9d4e45
Kimon Kukulis <kimon.kukulis@gmail.com>
2024-10-16 02:18:35 +0300

    refactoring paging.c

--- src/mem/paging.c
+++ src/mem/paging.c
@@ -21,6 +21,47 @@ uint64_t align_to_page(uint64_t address)
   return address & PAGE_MASK;
 }
 
+void print_page(uint64_t *pagedir, uint64_t index, uint8_t type, uint64_t offset)
+{
+  printf("%i: 0x%x: ", index, (void *)(&pagedir) - offset);
+
+  // pml4 and pdpt
+  if (type == 0 || type == 1)
+  {
+    struct PML4_entry *entry = (struct PML4_entry *)&pagedir;
+    printf("p: %i, rw: %i, u: %i, pwt: %i, pcd: %i, acc: %i, ps: %i, addr: 0x%x, nx: %i",
+           entry->present,
+           entry->rw,
+           entry->user_supervisor,
+           entry->pwt,
+           entry->pcd,
+           entry->accessed,
+           entry->ps,
+           entry->pdt_addr << 12,
+           entry->nx);
+  }
+
+  // pd and pt
+  if (type == 2 || type == 3)
+  {
+    struct Pagetable_entry *entry = (struct Pagetable_entry *)&pagedir;
+    printf("p: %i, rw: %i, u: %i, pwt: %i, pcd: %i, acc: %i, d: %i, p %i, g %i, addr: 0x%x, nx: %i",
+           entry->present,
+           entry->rw,
+           entry->user_supervisor,
+           entry->pwt,
+           entry->pcd,
+           entry->accessed,
+           entry->dirty,
+           entry->pat,
+           entry->global,
+           entry->pdt_addr << 12,
+           entry->nx);
+  }
+
+  printf("\n");
+}
+
 void print_pagedir(uint64_t *pagedir, const char *name, uint32_t start, uint32_t count, uint8_t type, uint64_t offset)
 {
   printf("%s\n", name);
@@ -28,32 +69,7 @@ void print_pagedir(uint64_t *pagedir, const char *name, uint32_t start, uint32_t
   {
     if (pagedir[i] != 0)
     {
-      printf("%i: 0x%x: ", i, (void *)(&pagedir[i]) - offset);
-
-      if (type == 0 || type == 1)
-      {
-        struct PML4_entry *entry = (struct PML4_entry *)&pagedir[i];
-        printf("p: %i, rw: %i, u: %i, pwt: %i, pcd: %i, acc: %i, ps: %i, addr: 0x%x, nx: %i", entry->present, entry->rw, entry->user_supervisor, entry->pwt, entry->pcd, entry->accessed, entry->ps, entry->pdt_addr << 12, entry->nx);
-      }
-
-      if (type == 2 || type == 3)
-      {
-        struct Pagetable_entry *entry = (struct Pagetable_entry *)&pagedir[i];
-        printf("p: %i, rw: %i, u: %i, pwt: %i, pcd: %i, acc: %i, d: %i, p %i, g %i, addr: 0x%x, nx: %i",
-               entry->present,
-               entry->rw,
-               entry->user_supervisor,
-               entry->pwt,
-               entry->pcd,
-               entry->accessed,
-               entry->dirty,
-               entry->pat,
-               entry->global,
-               entry->pdt_addr << 12,
-               entry->nx);
-      }
-
-      printf("\n");
+      print_page(pagedir[i], i, type, offset);
     }
   }
   printf("\n");
@@ -121,6 +137,15 @@ void setup_paging()
   printf("paddr: %p\n", paddr);
 }
 
+uint64_t *get_next_table_entry(uint64_t *current_table, uint64_t index)
+{
+  if (current_table == NULL || !(current_table[index] & PRESENT_BIT))
+  {
+    return NULL; // Not a valid table
+  }
+  return (uint64_t *)(current_table[index] & PAGE_MASK);
+}
+
 void *get_physical_address(uint64_t virtual_address)
 {
   uint64_t pml4_index = (virtual_address >> 39) & ADDRESS_MASK;
@@ -130,18 +155,23 @@ void *get_physical_address(uint64_t virtual_address)
   uint64_t offset = virtual_address & PAGE_MASK;
 
   uint64_t *pml4_table = (uint64_t *)read_cr3();
-  uint64_t pdpt_address = pml4_table[pml4_index];
+  uint64_t *pdpt_table = get_next_table_entry(pml4_table, pml4_index);
+  uint64_t *pd_table = get_next_table_entry(pdpt_table, pdpt_index);
+  uint64_t *pt_table = get_next_table_entry(pd_table, pd_index);
 
-  uint64_t *pdpt_table = (uint64_t *)(pdpt_address & PAGE_MASK);
-  uint64_t pd_address = pdpt_table[pdpt_index];
-
-  uint64_t *pd_table = (uint64_t *)(pd_address & PAGE_MASK);
-  uint64_t pt_address = pd_table[pd_index];
-
-  uint64_t *pt_table = (uint64_t *)(pt_address & PAGE_MASK);
   uint64_t physical_address = (pt_table[pt_index] & PAGE_MASK) | offset;
+  return (void *)physical_address;
+}
 
-  return physical_address;
+void setup_next_page_table(uint64_t *table_entry, uint64_t **next_entry, uint8_t flags)
+{
+  if (!(*table_entry & PRESENT_BIT))
+  {
+    uint64_t paddr = allocate_physical_page();
+    *table_entry = paddr | flags;
+  }
+
+  *next_entry = (uint64_t *)(*table_entry & PAGE_MASK);
 }
 
 void map_page(uint64_t virtual_address, uint64_t physical_address, uint64_t *pml4_table, uint8_t flags)
@@ -154,26 +184,13 @@ void map_page(uint64_t virtual_address, uint64_t physical_address, uint64_t *pml
   uint64_t pd_index = (virtual_address >> 21) & ADDRESS_MASK;
   uint64_t pt_index = (virtual_address >> 12) & ADDRESS_MASK;
 
-  if (!(pml4_table[pml4_index] & PRESENT_BIT))
-  {
-    uint64_t pdpt = allocate_physical_page();
-    pml4_table[pml4_index] = pdpt | flags;
-  }
-  uint64_t *pdpt_table = (uint64_t *)(pml4_table[pml4_index] & PAGE_MASK);
+  uint64_t *pdpt_table = NULL;
+  uint64_t *pd_table = NULL;
+  uint64_t *pt_table = NULL;
 
-  if (!(pdpt_table[pdpt_index] & PRESENT_BIT))
-  {
-    uint64_t pd = allocate_physical_page();
-    pdpt_table[pdpt_index] = pd | flags;
-  }
-  uint64_t *pd_table = (uint64_t *)(pdpt_table[pdpt_index] & PAGE_MASK);
-
-  if (!(pd_table[pd_index] & PRESENT_BIT))
-  {
-    uint64_t pt = allocate_physical_page();
-    pd_table[pd_index] = pt | flags;
-  }
-  uint64_t *pt_table = (uint64_t *)(pd_table[pd_index] & PAGE_MASK);
+  setup_next_page_table(&pml4_table[pml4_index], &pdpt_table, flags);
+  setup_next_page_table(&pdpt_table[pdpt_index], &pd_table, flags);
+  setup_next_page_table(&pd_table[pd_index], &pt_table, flags);
 
   pt_table[pt_index] = physical_address | flags;
 }
