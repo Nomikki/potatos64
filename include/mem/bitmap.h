99c1a979d23d7302769c669ba80fdcc26f93abb9
Kimon Kukulis <kimon.kukulis@gmail.com>
2024-10-15 16:53:46 +0300

    bitmnap: check if page is free or used, allocate physical page and set range.

--- include/mem/bitmap.h
+++ include/mem/bitmap.h
@@ -4,6 +4,11 @@
 #include <types.h>
 
 extern uint64_t *bitmap_init(uint32_t size);
-extern void bitmap_set_page(uint64_t page_address, bool present);
-extern int bitmap_test(uint64_t address);
+// extern void bitmap_set_page(uint64_t page_address, bool present);
+// extern int bitmap_test(uint64_t address);
+extern int is_page_free(uint64_t page_index);
+extern void set_page_used(uint64_t page_index);
+extern uint64_t allocate_physical_page();
+extern void set_page_range_used(uint64_t memory_range_start, uint64_t memory_range_end);
+
 #endif
--- src/mem/bitmap.c
+++ src/mem/bitmap.c
@@ -1,7 +1,7 @@
 #include <mem/bitmap.h>
 #include <stdio.h>
 
-// extern uint64_t _kernel_physical_end;
+extern uint64_t _kernel_physical_end;
 extern uint64_t _kernel_end;
 
 uint16_t *bitmap = NULL;
@@ -9,10 +9,14 @@ uint16_t *bitmap = NULL;
 #define BITS_PER_ROW 16
 #define PAGE_SIZE 4096
 
+int total_pages = 0;
+
 uint64_t *bitmap_init(uint32_t size)
 {
 
+  // debug
   printf("Bitmap: %u bytes\n", size);
+
   bitmap = (uint16_t *)&_kernel_end;
 
   for (int i = 0; i < size; i++)
@@ -20,33 +24,53 @@ uint64_t *bitmap_init(uint32_t size)
     bitmap[i] = 0;
   }
 
+  uint64_t amount_of_pages = (uint64_t)(&_kernel_physical_end) / PAGE_SIZE;
+  printf("kernel end: %u\n", amount_of_pages);
+
+  set_page_range_used(0, (uint64_t)(&_kernel_physical_end) + 0x1000);
+
+  total_pages = size * BITS_PER_ROW;
+
   return bitmap;
 }
 
-void bitmap_set_page(uint64_t page_address, bool present)
+int is_page_free(uint64_t page_index)
 {
-  uint64_t bitmap_location = page_address / PAGE_SIZE;
+  return !(bitmap[page_index / BITS_PER_ROW] & (1 << (page_index % BITS_PER_ROW)));
+}
 
-  uint64_t row = bitmap_location / (BITS_PER_ROW);
-  uint64_t column = bitmap_location % (BITS_PER_ROW);
-  if (present)
+void set_page_used(uint64_t page_index)
+{
+  bitmap[page_index / BITS_PER_ROW] |= (1 << (page_index % BITS_PER_ROW));
+}
+
+void set_page_range_used(uint64_t memory_range_start, uint64_t memory_range_end)
+{
+  uint64_t page_start = memory_range_start / PAGE_SIZE;
+  uint64_t page_end = memory_range_end / PAGE_SIZE;
+
+  // debug
+  // printf("bitmap range: %x:%x = %u:%u\n", memory_range_start, memory_range_end, page_start, page_end);
+
+  for (int i = page_start; i < page_end; i++)
   {
-    bitmap[row] |= 1 << column;
-  }
-  else
-  {
-    bitmap[row] &= ~(1 << column);
+    set_page_used(i);
   }
 }
 
-int bitmap_test(uint64_t address)
+uint64_t allocate_physical_page()
 {
-  uint64_t bitmap_location = address / PAGE_SIZE;
-  uint64_t column = bitmap_location % BITS_PER_ROW;
-
-  int val = (bitmap[bitmap_location / BITS_PER_ROW] >> column) & 1;
-  // printf("test: column: %i, final: %b, val: %i\n", column, bitmap[bitmap_location], val);
-
-  // uint64_t column = bitmap_location & BITS_PER_ROW;
-  return val;
+  uint64_t return_addr = 0;
+  for (uint64_t i = 0; i < total_pages; i++)
+  {
+    if (is_page_free(i))
+    {
+      set_page_used(i);
+      return_addr = i * PAGE_SIZE;
+      break;
+    }
+  }
+  // debug
+  // printf("allocate: %x\n", return_addr);
+  return return_addr;
 }
