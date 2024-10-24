3ca1f9bc2d7a6a7c81c5dd308f4f489a3f81d7c8
Kimon Kukulis <kimon.kukulis@gmail.com>
2024-10-24 12:33:43 +0300

    strcpy, memcpy, memmove and memset added to string.h/.c

--- include/string.h
+++ include/string.h
@@ -1,6 +1,32 @@
 #ifndef STRING_H_
 #define STRING_H_
 
+#include <types.h>
+
 extern int strlen(const char *str);
+extern char *strcpy(char *destination, const char *source);
+
+/*
+  Copy block of memory
+  Copies the values of num bytes from the location pointed to
+  by source directly to the memory block pointed to by destination.
+*/
+extern void *memcpy(void *destination, const void *source, size_t num);
+
+/*
+  Move block of memory
+  Copies the values of num bytes from the location pointed by
+  source to the memory block pointed by destination. Copying takes
+  place as if an intermediate buffer were used, allowing the destination
+  and source to overlap.
+*/
+extern void *memmove(void *destination, const void *source, size_t num);
+
+/*
+  Fill block of memory
+  Sets the first num bytes of the block of memory pointed by ptr to
+  the specified value(interpreted as an unsigned char)
+*/
+extern void *memset(void *ptr, int value, size_t num);
 
 #endif // STRING_H_
--- src/lib/string.c
+++ src/lib/string.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include <mem/kheap.h>
+#include <stdio.h>
 
 int strlen(const char *str)
 {
@@ -6,4 +8,68 @@ int strlen(const char *str)
   while (str[i] != '\0')
     i++;
   return i;
+}
+
+char *strcpy(char *destination, const char *source)
+{
+  int len = strlen(source);
+  for (int i = 0; i < len; i++)
+  {
+    destination[i] = source[i];
+  }
+  destination[len] = 0;
+  return destination;
+}
+
+void *memcpy(void *destination, const void *source, size_t num)
+{
+  // yep, its not optimized at all :-D
+  uint8_t *d = (uint8_t *)destination;
+  const uint8_t *s = (uint8_t *)source;
+  uint64_t len = num;
+
+  for (size_t i = 0; i < len; i++)
+  {
+    d[i] = s[i];
+  }
+
+  return destination;
+}
+
+void *memmove(void *destination, const void *source, size_t num)
+{
+  // yep, its not optimized at all :-D
+  uint8_t *d = (uint8_t *)destination;
+  const uint8_t *s = (uint8_t *)source;
+  uint64_t len = num;
+
+  uint8_t *temp = kmalloc(num);
+
+  for (size_t i = 0; i < len; i++)
+  {
+    temp[i] = s[i];
+  }
+
+  for (size_t i = 0; i < len; i++)
+  {
+    d[i] = temp[i];
+  }
+
+  kfree(temp);
+  temp = NULL;
+
+  return destination;
+}
+
+void *memset(void *ptr, int value, size_t num)
+{
+  uint8_t *destination = (uint8_t *)ptr;
+  uint64_t len = num;
+
+  for (uint64_t i = 0; i < len; i++)
+  {
+    destination[i] = value;
+  }
+
+  return ptr;
 }
